97cee8326c4cb318b43083775dfd7200ee5cb151
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-08 13:23:14 +0200

    Added properties with accessor restrictions to Parking

--- CoolParking.BL/Models/Parking.cs
+++ CoolParking.BL/Models/Parking.cs
@@ -1,3 +1,18 @@
 ﻿// TODO: implement class Parking.
 //       Implementation details are up to you, they just have to meet the requirements 
 //       of the home task and be consistent with other classes and tests.
+using System.Collections.Generic;
+
+namespace CoolParking.BL.Models
+{
+    public class Parking
+    {
+        public decimal Balance { get; internal set; }
+        public IList<Vehicle> Vehicles { get; }
+
+        public Parking()
+        {
+            Vehicles = new List<Vehicle>();
+        }
+    }
+}
