75540b107d9e579c332ccb15b0c88e9e91b4b3b7
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-08 23:57:51 +0200

    Created placeholders of ITimerService methods for TimerService

--- CoolParking.BL/Services/TimerService.cs
+++ CoolParking.BL/Services/TimerService.cs
@@ -1,2 +1,34 @@
 ﻿// TODO: implement class TimerService from the ITimerService interface.
 //       Service have to be just wrapper on System Timers.
+
+using System.Timers;
+using CoolParking.BL.Interfaces;
+using CoolParking.BL.Models;
+using Microsoft.VisualBasic;
+
+namespace CoolParking.BL.Services
+{
+    public class TimerService : ITimerService
+    {
+        private double _interval;
+
+        public event ElapsedEventHandler Elapsed;
+
+        public double Interval { get; set; }
+
+        public void Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
