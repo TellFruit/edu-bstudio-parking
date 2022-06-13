583d6d69ab46d479534719d302dfb820d87dff5b
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-13 20:32:45 +0200

    Created VehiclesController + added ctor to it

---
+++ CoolParking/CoolParking.WebAPI/Controllers/VehiclesController.cs
@@ -0,0 +1,17 @@
+﻿using CoolParking.BL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoolParking.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiclesController : ControllerBase
+    {
+        private readonly IParkingService _parking;
+        public VehiclesController(IParkingService parking)
+        {
+            _parking = parking;
+        }
+    }
+}
