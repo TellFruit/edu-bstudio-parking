9f4838cdbfc8e6325ae06e29918b8a0c6bfd6aba
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-13 23:27:46 +0200

    Added Post request handler to Vehicle controller

--- CoolParking/CoolParking.WebAPI/Controllers/VehiclesController.cs
+++ CoolParking/CoolParking.WebAPI/Controllers/VehiclesController.cs
@@ -1,7 +1,10 @@
-﻿using CoolParking.BL.Interfaces;
+﻿using System.Net;
+using CoolParking.BL.Interfaces;
+using CoolParking.BL.Models;
 using CoolParking.WebAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CoolParking.WebAPI.Controllers
 {
@@ -13,6 +16,49 @@ namespace CoolParking.WebAPI.Controllers
         public VehiclesController(IParkingService parking)
         {
             _parking = parking;
+        }
+
+
+        /*
+         * POST api/vehicles
+           
+           Request:
+           Body schema: { “id”: string, “vehicleType”: int, “balance”: decimal }
+           Body example: { “id”: “LJ-4812-GL”, “vehicleType”: 2, “balance”: 100 }
+           Response:
+           If body is invalid - Status Code: 400 Bad Request
+           If request is handled successfully
+           Status Code: 201 Created
+           Body schema: { “id”: string, “vehicleType”: int, "balance": decimal }
+           Body example: { “id”: “LJ-4812-GL”, “vehicleType”: 2, "balance": 100 }
+         */
+
+        [HttpPost]
+        public IActionResult Post(ApiVehicle apiVehicle)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Vehicle vehicle = new Vehicle(apiVehicle.Id, apiVehicle.VehicleType, apiVehicle.Balance);
+                    
+                    _parking.AddVehicle(vehicle);
+
+                    string json = JsonConvert.SerializeObject(vehicle);
+
+                    return Created(Url.Link("DefaultApi", new {id = vehicle.Id}) ?? string.Empty, json);
+                }
+                else
+                {
+                    throw new InvalidOperationException("Inconsistent json body. Required field: string Id, VehicleType VehicleType, decimal Balance");
+                }
+            }
+            // no exception specification because every of them described inside of the classes
+            // lead to the same status code response 
+            catch (Exception e)
+            {
+                return BadRequest(new {message = e.Message});
+            }
         }
     }
 }
