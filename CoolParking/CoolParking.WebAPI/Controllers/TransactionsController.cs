080951bc7a0135e8051c3ccdf4b0e2315132362e
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-14 13:00:57 +0200

    Added LastTransactions response handler to return last parking transactions

--- CoolParking/CoolParking.WebAPI/Controllers/TransactionsController.cs
+++ CoolParking/CoolParking.WebAPI/Controllers/TransactionsController.cs
@@ -14,5 +14,18 @@ namespace CoolParking.WebAPI.Controllers
         {
             _parking = parking;
         }
+
+        /*
+         * GET api/transactions/last
+           
+           Response:
+           If request is handled successfully
+           Status Code: 200 OK
+           Body schema: [{ “vehicleId”: string, “sum”: decimal, "transactionDate": DateTime }]
+           Body example: [{ “vehicleId”: "DG-3024-UB", “sum”: 3.5, "transactionDate": "2020-05-10T11:36:20.6395402+03:00"}]
+         */
+
+        [HttpGet("last")]
+        public IActionResult LastTransactions() => Ok(_parking.GetLastParkingTransactions());
     }
 }
