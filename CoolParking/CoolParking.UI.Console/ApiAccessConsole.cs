635c7c2c51711b64820d993aef7485821ba0cc94
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-14 15:39:12 +0200

    Added HttpClient variable to ApiAccessConsole + ctor

--- CoolParking/CoolParking.UI.Console/ApiAccessConsole.cs
+++ CoolParking/CoolParking.UI.Console/ApiAccessConsole.cs
@@ -8,5 +8,12 @@ namespace CoolParking.UI.Console
 {
     internal class ApiAccessConsole
     {
+        private HttpClient _client;
+
+        public ApiAccessConsole(HttpClient client, string baseAddress)
+        {
+            _client = client;
+            _client.BaseAddress = new Uri(baseAddress);
+        }
     }
 }
