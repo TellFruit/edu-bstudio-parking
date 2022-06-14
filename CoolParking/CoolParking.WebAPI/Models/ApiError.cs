53738306f73f33d5ee4547cb2172ab0c5bd20c7c
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-14 12:05:41 +0200

    Added simple error model for api request handling

---
+++ CoolParking/CoolParking.WebAPI/Models/ApiError.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace CoolParking.WebAPI.Models
+{
+    public class ApiError
+    {
+        public int StatusCode { get; }
+
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Message { get; private set; }
+
+        public ApiError(int statusCode, string message)
+        { 
+            StatusCode = statusCode;
+            Message = message;
+        }
+    }
+}
