f7d5ede4dc0c35caf4f6e1a0d562d46349cff16f
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-14 13:00:16 +0200

    Created Transactions controller to handle transactions requests

---
+++ CoolParking/CoolParking.WebAPI/Controllers/TransactionsController.cs
@@ -0,0 +1,18 @@
+﻿using CoolParking.BL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoolParking.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly IParkingService _parking;
+
+        public TransactionsController(IParkingService parking)
+        {
+            _parking = parking;
+        }
+    }
+}
