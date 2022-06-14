ce29ab44a2b4c555d500802cbd64fe32aad9bc56
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-14 09:11:27 +0200

    Filled ApiVehicle with properties and attributes

--- CoolParking/CoolParking.WebAPI/Models/ApiVehicle.cs
+++ CoolParking/CoolParking.WebAPI/Models/ApiVehicle.cs
@@ -1,6 +1,15 @@
-﻿namespace CoolParking.WebAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using CoolParking.BL.Models;
+
+namespace CoolParking.WebAPI.Models
 {
     public class ApiVehicle
     {
+        [Required]
+        public string Id { get; }
+        [Required]
+        public VehicleType VehicleType { get; }
+        [Required]
+        public decimal Balance { get; internal set; }
     }
 }
