657f2db216e7cb734940781a75dce93e5185036c
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-14 15:43:05 +0200

    Added GetBalance request method to ApiAccessConsole

--- CoolParking/CoolParking.UI.Console/ApiAccessConsole.cs
+++ CoolParking/CoolParking.UI.Console/ApiAccessConsole.cs
@@ -3,17 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace CoolParking.UI.Console
 {
     internal class ApiAccessConsole
     {
-        private HttpClient _client;
+        private readonly HttpClient _client;
 
         public ApiAccessConsole(HttpClient client, string baseAddress)
         {
             _client = client;
             _client.BaseAddress = new Uri(baseAddress);
+        }
+
+        public async Task<decimal> GetBalance()
+        {
+            var response = await _client.GetAsync("/parking" + "/balance");
+            var balance = await response.Content.ReadAsStringAsync();
+            var reader = new JsonTextReader(new StringReader(balance));
+            reader.FloatParseHandling = FloatParseHandling.Decimal;
+            Task<decimal?> result = reader.ReadAsDecimalAsync();
+
+            return result.Result ?? -1;
         }
     }
 }
